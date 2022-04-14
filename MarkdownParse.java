503b836969969823fb1bbdcb85f89f2e3337595a
jt-ucsd <103224763+jt-ucsd@users.noreply.github.com>
2022-04-14 16:49:19 -0700

    Update MarkdownParse.java
    Fix another bug

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -23,8 +23,17 @@ public class MarkdownParse {
                 }
             }
             int closeBracket = markdown.indexOf("](", openBracket);
+            if(closeBracket == -1) {
+                break;
+            }
             int openParen = markdown.indexOf("(", closeBracket);
+            if(openParen == -1) {
+                break;
+            }
             int closeParen = markdown.indexOf(")", openParen);
+            if(closeParen == -1) {
+                break;
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
