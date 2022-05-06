aade8e55119b8781c2026ad4a1c5bcd1bef2dec6
Justin Truong <cs15lsp22atu@ieng6-202.ucsd.edu>
2022-05-05 17:23:32 -0700

    Lab Report 3

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -13,27 +13,9 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
-            if(openBracket == -1) {
-                break;
-            }
-            if(openBracket > 0) {
-                if(markdown.charAt(openBracket - 1) == '!') {
-                    currentIndex = openBracket + 1;
-                    continue;
-                }
-            }
             int closeBracket = markdown.indexOf("]", openBracket);
-            if(closeBracket == -1) {
-                break;
-            }
             int openParen = markdown.indexOf("(", closeBracket);
-            if(openParen == -1) {
-                break;
-            }
             int closeParen = markdown.indexOf(")", openParen);
-            if(closeParen == -1) {
-                break;
-            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
